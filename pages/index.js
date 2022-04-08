7430c14dcb7ff62e139b74af64787b90f5907822
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-08 15:41:48 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-neutral-900 text-white flex items-center justify-center">
+      Pattern Maker v0.0.1
+    </div>
+  );
+}
