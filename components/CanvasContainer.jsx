377e17c57d64ceeee43bf057dbeaf2db26969324
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-09 13:57:47 -0400

    canvas container update to track elements and mouse position

--- components/CanvasContainer.jsx
+++ components/CanvasContainer.jsx
@@ -1,5 +1,84 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Element from "./Element";
 
 export default function CanvasContainer() {
-  return <div className="canvascontainer">Pattern Maker v0.0.1</div>;
+  const canvasRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [elements, setElements] = React.useState([
+    {
+      id: 1,
+      position: { x: 100, y: 100 },
+      size: { width: 100, height: 100 },
+      rotation: 0,
+      selected: false,
+    },
+  ]);
+  const [selectedElement, setSelectedElement] = React.useState(null);
+  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
+  const [delta, setDelta] = React.useState({ x: 0, y: 0 });
+
+  const handleDragStart = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    // if (!isDragging) return;
+    const { x, y } = e.target.getBoundingClientRect();
+    setMousePosition({ x: e.clientX, y: e.clientY });
+    setDelta({ x: 0, y: 0 });
+  };
+
+  const handleDragEnd = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    console.log("drag end");
+    setIsDragging((prev) => false);
+  };
+
+  const handleDrag = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    if (!isDragging) return;
+
+    // const { x, y } = e.target.getBoundingClientRect();
+    // console.log("Clicked on: ", x, y);
+
+    // how far mouse has moved from the start of the drag
+    const deltaX = e.clientX - mousePosition.x;
+    const deltaY = e.clientY - mousePosition.y;
+
+    // setMousePosition({ x: e.clientX, y: e.clientY });
+    setDelta({ x: deltaX, y: deltaY });
+  };
+
+  return (
+    <div
+      className="relative w-full h-full flex-grow bg-neutral-900 overflow-hidden"
+      onMouseMove={handleDrag}
+      onMouseUp={handleDragEnd}
+      onMouseDown={(e) => {
+        handleDragStart(e);
+      }}
+      // onClick={handleClick}
+    >
+      {
+        // show each Element in the array
+        elements.map((element, i) => {
+          return (
+            <Element
+              key={i}
+              isDragging={selectedElement === i}
+              setIsDragging={setIsDragging}
+              mousePosition={delta}
+              onClick={() => setSelectedElement(i)}
+            />
+          );
+        })
+      }
+      <canvas
+        ref={canvasRef}
+        width={`100%`}
+        height={`100%`}
+        className="block w-full h-full"
+      />
+    </div>
+  );
 }
