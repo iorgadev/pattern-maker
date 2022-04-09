cf945ab432e571915bcc9ae53cccc0787b05a4ac
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-09 14:06:16 -0400

    mockup of panel with icon search results

--- components/OptionsPanel.jsx
+++ components/OptionsPanel.jsx
@@ -1,5 +1,49 @@
 import React from "react";
+import SearchBar from "@/components/OptionsPanel/SearchBar";
+import SearchOptions from "@/components/OptionsPanel/SearchOptions";
 
 export default function OptionsPanel() {
-  return <div className="optionspanel">OptionsPanel</div>;
+  const [iconResults, setIconResults] = React.useState([]);
+
+  // make an array from 1 to 42
+  const icons = Array.from(Array(40).keys()).map((i) => i + 1);
+
+  const getIcons = async (query) => {
+    const response = await fetch("/api/getIcons", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        q: query,
+      }),
+    });
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log(data);
+      setIconResults(data);
+      return;
+    }
+    setIconResults([]);
+  };
+
+  return (
+    <div className="optionspanel">
+      <SearchBar />
+      <SearchOptions />
+
+      <div className="flex-grow w-full grid grid-cols-4 gap-6 mt-10 px-4">
+        {icons.map((icon, i) => {
+          return (
+            <div
+              key={i}
+              className="w-16 h-16 border-2 border-blue-100 border-opacity-50 p-1"
+            >
+              <img src={`/images/icons/${i}.png`} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
